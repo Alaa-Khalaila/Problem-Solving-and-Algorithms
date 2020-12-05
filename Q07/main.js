b5a490ce1c41fd550829dbffcf626b39dbd2d801
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-06 01:22:36 +0200

    Q7 solved

--- Q07/main.js
+++ Q07/main.js
@@ -6,13 +6,20 @@ console.log('Problem Solving Q: 7');
 Write a function that returns the sum of odd numbers in an array.
 */
 
-function SumOddNumber() {
-  // YOUR CODE HERE
+function SumOddNumber(arr) {
+  let sum=0
+  arr.forEach(e=>{
+    if(e % 2 !== 0){
+      sum += e
+    }
+  })
+  return sum
 }
 
-/* 
-Examples:
-SumOddNumber([1, 2, 3, 4, 5]) // => 9
-SumOddNumber([1, 5, 3, 4, 5]) // => 14
-SumOddNumber([12, 2, 3, 4, 5, 0]) // => 8
-*/
+
+res1=SumOddNumber([1, 2, 3, 4, 5]) // => 9
+res2=SumOddNumber([1, 5, 3, 4, 5]) // => 14
+res3=SumOddNumber([12, 2, 3, 4, 5, 0]) // => 8
+
+console.log(res1,res2,res3)
+
