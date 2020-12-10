94a850315261663fdc07ce7d16db8e173eb9fe8f
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-11 01:02:27 +0200

    Q18 solved

--- Q18/main.js
+++ Q18/main.js
@@ -7,13 +7,17 @@ Given an integer, n, return n! (n factorial)
 5! = 5 * 4 * 3 * 2 * 1
 */
 
-function factorial() {
-  // YOUR CODE HERE
+function factorial(n) {
+  let nFact =1
+  for(i=n;i>0;i--){
+    nFact = nFact*i 
+  }
+  return nFact
 }
 
-/* 
-Examples:
-factorial(1) // => 1
-factorial(3) // => 6
-factorial(4) // => 24
-*/
+
+res1 =factorial(1) // => 1
+res2 =factorial(3) // => 6
+res3 =factorial(4) // => 24
+
+console.log(res1,res2,res3)
