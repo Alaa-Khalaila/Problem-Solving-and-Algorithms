ac93af1a4aa73533cca7c66a80abda5e1fc1feea
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-10 04:41:08 +0200

    Q10 solved like a boss x2

--- Q10/main.js
+++ Q10/main.js
@@ -6,14 +6,23 @@ console.log('Problem Solving Q: 10');
 Write a function that returns the number of occurrences of the most frequent character in a sentence.
 */
 
-function timesOfMostFreqChar() {
-  // YOUR CODE HERE
+function timesOfMostFreqChar(str) {
+  let sortStr = str.split('').sort()
+  let lognest = 0;
+  let test = 0;
+  for(i=0;i<sortStr.length;i++){
+    test = sortStr.filter(e=>e===sortStr[i]).length
+    if(test>lognest){
+      lognest=test
+    }
+  }
+  return lognest
 }
 
-/* 
-Examples:
-answer1('hello world') // => 3
-answer1('hello world lol') // => 5
-answer1('hello world lol woooow') // => 7
-answer1('hello world lol wow www.com') // => 6
-*/
+
+res1=timesOfMostFreqChar('hello world') // => 3
+res2 =timesOfMostFreqChar('hello world lol') // => 5
+res3 =timesOfMostFreqChar('hello world lol woooow') // => 7
+res4 =timesOfMostFreqChar('hello world lol wow www.com') // => 6
+
+console.log(res1,res2,res3,res4)
