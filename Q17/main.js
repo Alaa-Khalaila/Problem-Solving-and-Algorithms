69d87d5fb7fac3c4407f2cc0b623ed557f16c8a7
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-11 00:45:44 +0200

    Q17 solved

--- Q17/main.js
+++ Q17/main.js
@@ -6,13 +6,18 @@ console.log('Problem Solving Q:17 ');
 Given an arbitrary input string, return the first non-repeating character. For strings with all repeats, return 'sorry'
 */
 
-function firstNonRepeatedCharacter() {
-  // YOUR CODE HERE
+function firstNonRepeatedCharacter(str) {
+  for (i=0;i<str.length;i++){
+    let arr = str.split('').filter(e=>e===str[i])
+    if(arr.length===1){
+      return arr[0]
+    }
+  }
 }
 
-/* 
-Examples:
-firstNonRepeatedCharacter('ABCDBIRDUP') // => 'A'
-firstNonRepeatedCharacter('ALABAMA') // => 'L'
-firstNonRepeatedCharacter('Uber for horses') // => 'U'
-*/
+
+res1 =firstNonRepeatedCharacter('ABCDBIRDUP') // => 'A'
+res2 =firstNonRepeatedCharacter('ALABAMA') // => 'L'
+res3 =firstNonRepeatedCharacter('Uber for horses') // => 'U'
+
+console.log(res1,res2,res3)
