0a3492d8f6948244c98200c2f7eb797855aa4806
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-10 05:28:22 +0200

    Q12 solved

--- Q12/main.js
+++ Q12/main.js
@@ -6,13 +6,19 @@ console.log('Problem Solving Q: 12');
 Write a function that reverses the characters of every word of a sentence. Input sentences contain only English letter and space characters.
 */
 
-function reverseEachWord() {
-  // YOUR CODE HERE
+function reverseEachWord(str) {
+  let arr =str.split(' ')
+  let lastArr=[];
+  for(i=0;i<arr.length;i++){
+    let word =arr[i].split('').reverse().join('');
+    lastArr.push(word)
+  }
+  return lastArr.join(" ");
 }
 
-/* 
-Examples:
-reverseEachWord("This is a test") // => "sihT si a tset"
-reverseEachWord("Lets code guys") // => "steL edoc syug"
-reverseEachWord("Jouza admire Fried Chicken") // => "azuoJ erimda deirF nekcihC"
-*/
+
+res1 =reverseEachWord("This is a test") // => "sihT si a tset"
+res2 =reverseEachWord("Lets code guys") // => "steL edoc syug"
+res3 =reverseEachWord("Jouza admire Fried Chicken") // => "azuoJ erimda deirF nekcihC"
+
+console.log(res1,res2,res3)
