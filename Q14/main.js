49eb8f735303d524d350288113faaf3fafdceee5
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-10 23:54:10 +0200

    Q14 solved

--- Q14/main.js
+++ Q14/main.js
@@ -7,13 +7,17 @@ Given an array of negative/positive integers, return the element in the center p
 If the array has an even number of elements, return the average of the two middle elements instead.
 */
 
-function ArrayMiddle() {
-  // YOUR CODE HERE
+function ArrayMiddle(arr) {
+  if(arr.length %2!==0){
+    return arr[Math.ceil(arr.length/2 -1)]
+  }else{
+    return (arr[(arr.length/2) -1] + arr[arr.length/2])/2
+  }
 }
 
-/* 
-Examples:
-ArrayMiddle([200,5,100]) // => 5
-ArrayMiddle([2,3,2,3,2]) // => 2
-ArrayMiddle([2,3]) // => 2.5
-*/
+
+res1 =ArrayMiddle([200,5,100]) // => 5
+res2 =ArrayMiddle([2,3,2,3,2]) // => 2
+res3 =ArrayMiddle([2,3]) // => 2.5
+
+console.log(res1,res2,res3)
