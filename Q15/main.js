2ffb9d3cfcc966ba513bfcbe47af01859a82c5c4
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-11 00:14:23 +0200

    Q15 solved

--- Q15/main.js
+++ Q15/main.js
@@ -8,13 +8,23 @@ Your function should return the common characters in the same order that they ap
 Do not return duplicate characters and ignore whitespace in your returned string.
 */
 
-function commonCharacters() {
-  // YOUR CODE HERE
+function commonCharacters(str1,str2) {
+  const arr1 = str1.split('');
+  const arr2 = str2.split('');
+  let word =''
+
+  for(i=0;i<arr1.length;i++){
+    let filterArr = arr2.filter(e=>e===arr1[i])
+    if(filterArr[0] && filterArr[0]!==' ' && !word.includes(filterArr[0]) ){
+      word= word + filterArr[0]
+    }
+  }
+  return word;
 }
 
-/* 
-Examples:
-commonCharacters('abc', 'abc') // => 'abc'
-commonCharacters('What is love?', "Baby don't hurt me") // => 'hatoe'
-commonCharacters('Riding on a buffalo makes me more approachable','so what') // => 'oash'
-*/
+
+res1 =commonCharacters('abc', 'abc') // => 'abc'
+res2 =commonCharacters('What is love?', "Baby don't hurt me") // => 'hatoe'
+res3 =commonCharacters('Riding on a buffalo makes me more approachable','so what') // => 'oash'
+
+console.log(res1,res2,res3)
