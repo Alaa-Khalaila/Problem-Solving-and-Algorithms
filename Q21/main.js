1c1b7096a1d8233aabde58db942a1e9c4d011002
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-11 18:00:07 +0200

    Q21 solved

--- Q21/main.js
+++ Q21/main.js
@@ -6,13 +6,21 @@ console.log('Problem Solving Q: 21');
 Given a string, remove any characters that are not unique from the string.
 */
 
-function onlyUnique() {
-  // YOUR CODE HERE
+function onlyUnique(str) {
+  let finalStr=''
+  let arr = str.split('')
+  for(i=0;i<arr.length;i++){
+    let filterArr  = arr.filter(e=>arr[i]===e)
+    if(filterArr.length===1){
+      finalStr = finalStr + filterArr[0]
+    }
+  }
+  return finalStr
 }
 
-/* 
-Examples:
-onlyUnique('hello there') // => 'o tr'
-onlyUnique('xyz') // => 'xyz'
-onlyUnique('iiii') // => ''
-*/
+
+res1 =onlyUnique('hello there') // => 'o tr'
+res2 =onlyUnique('xyz') // => 'xyz'
+res3 =onlyUnique('iiii') // => ''
+
+console.log(res1,res2,res3)
