b5c9850630169afa91b0140fce2404865caf5801
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-12 02:51:19 +0200

    Q26 solved

--- Q26/main.js
+++ Q26/main.js
@@ -6,12 +6,16 @@ console.log('Problem Solving Q:26 ');
 Write a JavaScript function to clone an array.
 */
 
-function array_Clone() {
-  // YOUR CODE HERE
+function array_Clone(arr) {
+  let clone = []
+  for(i=0;i<arr.length;i++){
+    clone[i]=arr[i]
+  }
+  return clone
 }
 
-/* 
-Examples:
-array_Clone ([1,2,3]) // =>[1,2,3]
-array_Clone ([1,9,8]) // =>[1,9,8]
-*/
+
+res1 =array_Clone ([1,2,3]) // =>[1,2,3]
+res2 =array_Clone ([1,9,8]) // =>[1,9,8]
+
+console.log(res1,res2)
