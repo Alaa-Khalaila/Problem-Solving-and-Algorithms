fe423f42cd3e9768f1c8f4cd3f8bf12593e92ca0
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-13 13:59:37 +0200

    Q3 solved

--- 02_Data Structures + Algorithms [27 Q]/exercises/03_reverseint/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/03_reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const parse = String(n).split('').reverse().join('')
+    if(String(n).split('').includes('-')){
+        if(String(n).split('').reverse() === '0'){
+            return -1 * parseInt(parse.shift())
+        };
+        return -1 *  parseInt(parse)
+    };
+   
+    return parseInt(parse)
+}
 
 module.exports = reverseInt;
