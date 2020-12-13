5784769955edeb003418e30e975576c4978ea562
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-13 14:15:41 +0200

    Q4 solved

--- 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
@@ -5,6 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let arr = str.split('')
+    let mostUsed =''
+    for(i = 0; i < arr.length; i++){
+       if(arr.filter(e=>e === arr[i].length >mostUsed.length)) {
+        mostUsed =arr[i]
+       }
+    }
+    return mostUsed
+    }
 
 module.exports = maxChar;
